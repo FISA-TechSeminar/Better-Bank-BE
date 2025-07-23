1655447088d5f60fd0fffd2c173e3a69d2c7ea05
unknown <ricky0130@naver.com>
2025-07-23 16:05:18 +0900

    [#1] test: 계좌 내역 더미데이터 추가

--- src/test/java/com/practice/thebetterbank/service/TransactionHistoryTest.java
+++ src/test/java/com/practice/thebetterbank/service/TransactionHistoryTest.java
@@ -26,12 +26,22 @@ public class TransactionHistoryTest {
     private AccountRepository accountRepository;
 
     private final Random random = new Random();
+    private static final List<String> NAMES = List.of("최홍석", "홍혜원", "황지환", "김혜경", "이제현", "김동민", "이정이");
+    private static final List<String> SHOPS = List.of("엽기떡볶이", "이디야커피", "GS25", "스타벅스", "배달의민족", "맘스터치", "던킨도너츠");
 
+    private String getRandomName() {
+        return NAMES.get(random.nextInt(NAMES.size()));
+    }
+
+    private String getRandomShop() {
+        return SHOPS.get(random.nextInt(SHOPS.size()));
+    }
     @Test
-    public void transactionHistoryInputDummys() {
+    public void transactionHistoryTest() {
+
         for (long accountId = 1; accountId <= 12; accountId++) {
             int count = (accountId <= 8) ? 10 : 1000;
-            Account account = accountRepository.findById(accountId).get();
+            Account account = accountRepository.findById(accountId).orElseThrow();
 
             List<TransactionHistory> histories = new ArrayList<>();
 
@@ -41,10 +51,21 @@ public class TransactionHistoryTest {
                 LocalDate date = LocalDate.now().minusDays(random.nextInt(90)); // 최근 3개월 랜덤
                 String targetAccount = "10001234" + String.format("%04d", random.nextInt(10000));
 
+                String transactionName;
+                if (type == TransactionType.TRANSFER) {
+                    if (random.nextBoolean()) {
+                        transactionName = getRandomName();
+                    } else {
+                        transactionName = getRandomShop();
+                    }
+                } else { // DEPOSIT
+                    transactionName = getRandomName();
+                }
+
                 TransactionHistory history = TransactionHistory.builder()
                         .account(account)
                         .amount(amount)
-                        .transactionName("더미거래-" + (i + 1))
+                        .transactionName(transactionName)
                         .transactionDate(date)
                         .targetAccountNumber(targetAccount)
                         .transactionType(type)
@@ -66,5 +87,6 @@ public class TransactionHistoryTest {
 
             System.out.println(accountId + "번 계좌 더미 데이터 " + count + "개 생성 완료");
         }
+
     }
 }
