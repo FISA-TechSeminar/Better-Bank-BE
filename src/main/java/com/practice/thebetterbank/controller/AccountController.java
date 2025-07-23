9b400a600dd9ef527c011cdd2db4c1b68e9388b8
menzzi <minmin3927@naver.com>
2025-07-23 17:25:26 +0900

    feat: Controller 메서드 반환 타입을 ResultDTO로 변경하여 일관된 API 응답 구조 적용

--- src/main/java/com/practice/thebetterbank/controller/AccountController.java
+++ src/main/java/com/practice/thebetterbank/controller/AccountController.java
@@ -1,9 +1,10 @@
 package com.practice.thebetterbank.controller;
 
+import com.practice.thebetterbank.controller.dto.ResultDTO;
 import com.practice.thebetterbank.entity.Account;
 import com.practice.thebetterbank.service.account.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +19,18 @@ public class AccountController {
 
     // 유저별 계좌 목록 조회
     @GetMapping("/members/{memberId}/accounts")
-    public ResponseEntity<List<Account>> getAccountsByMemberId(@PathVariable Long memberId) {
+    public ResultDTO<List<Account>> getAccountsByMemberId(@PathVariable Long memberId) {
         List<Account> accounts = accountService.getAccountsByMemberId(memberId);
-        return ResponseEntity.ok(accounts);
+
+        return ResultDTO.res(HttpStatus.OK, "계좌 목록 조회 성공", accounts);
     }
 
     // 계좌 상세 조회
     @GetMapping("/accounts/{accountId}")
-    public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) {
+    public ResultDTO<Account> getAccountById(@PathVariable Long accountId) {
+
         return accountService.getAccountById(accountId)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+                .map(account -> ResultDTO.res(HttpStatus.OK, "계좌 상세 조회 성공", account))
+                .orElse(ResultDTO.res(HttpStatus.NOT_FOUND, "계좌를 찾을 수 없습니다."));
     }
 }
